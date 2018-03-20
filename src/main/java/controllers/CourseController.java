a1647f8c668b9e1a96593ed43e23f5d03eb52ca7
Jim Bevington <jim.bevington@hotmail.com>
2018-03-20 16:01:43 +0000

    created courses/create form template and started on post university/courses route

--- src/main/java/controllers/CourseController.java
+++ src/main/java/controllers/CourseController.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class CourseController {
 
@@ -32,12 +33,26 @@ public class CourseController {
         }, new VelocityTemplateEngine());
 
 //        create new
-        get("/university/courses/new", (req, res) -> {
+        get("/university/courses/create", (req, res) -> {
 
             HashMap<String, Object> model = new HashMap<>();
             model.put("template", "templates/courses/create.vtl");
 
             return new ModelAndView(model, "templates/layout.vtl");
+
+        }, new VelocityTemplateEngine());
+
+        post("/university/courses", (req, res) -> {
+
+            String title = req.queryParams("title");
+            String level = req.queryParams("level");
+            String startDate = req.queryParams("startDate");
+            String endDate= req.queryParams("endDate");
+
+            Course course = new Course(title, level, )
+
+            res.redirect("university/courses");
+            return null;
 
         }, new VelocityTemplateEngine());
 
